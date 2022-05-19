42304e9f58aacf4fa0816971a332e661cd6ab9bb
unknown <patidaranil0791@gmail.com>
2022-05-19 11:25:32 +0530

    update

--- Utils/jwtToken.js
+++ Utils/jwtToken.js
@@ -11,7 +11,7 @@ const sendToken = (user, statusCode, res) => {
       Date.new() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 //day*hour*minute*second
     ),
     httpOnly: true,
-  };
+  }; 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     user,
